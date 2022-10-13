4aa55a51195d6da3aa5091f3b01a83345c744946
rjservidor04 <73457698+rjservidor04@users.noreply.github.com>
2022-10-13 14:08:14 +0800

    Register Module Update

--- PicSellApplication/app/src/main/java/com/example/picsellapplication/RegisterController.java
+++ PicSellApplication/app/src/main/java/com/example/picsellapplication/RegisterController.java
@@ -1,12 +1,21 @@
 package com.example.picsellapplication;
 
 public class RegisterController {
+    boolean registerStatus;
 
-    public boolean checkDuplicate(String storeName, String storeOwnerName, String userName){
+    public boolean checkDuplicate(UserModel userModel, String storeName, String storeOwnerName, String username){
+        registerStatus = !userModel.checkStoreDuplicateFromDB(storeName, storeOwnerName) || !userModel.checkUsernameDuplicateFromDB(username);
 
+        return registerStatus;
     }
 
     public boolean passwordConfirmation(String password, String confirmPassword){
+        registerStatus = password.equals(confirmPassword);
 
+        return registerStatus;
+    }
+
+    public void registerAccount(UserModel userModel, String storeName, String storeOwnerName, String username, String password){
+        userModel.createAccount(storeName, storeOwnerName, username, password);
     }
 }
--- PicSellApplication/app/src/main/java/com/example/picsellapplication/RegisterView.java
+++ PicSellApplication/app/src/main/java/com/example/picsellapplication/RegisterView.java
@@ -2,16 +2,16 @@ package com.example.picsellapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
-import android.app.Activity;
 import android.content.Intent;
-import android.content.SharedPreferences;
 import android.view.View;
-import android.widget.Button;
 import android.widget.EditText;
-import android.widget.TextView;
 import android.widget.Toast;
 
 public class RegisterView extends AppCompatActivity{
+    UserModel userModel = new UserModel(RegisterView.this);
+    RegisterController registerController;
+    EditText txtStoreName, txtStoreOwnerName, txtUsername, txtPassword, txtConfirmPassword;
+    String storeName, storeOwnerName, username, password, confirmPassword;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +19,35 @@ public class RegisterView extends AppCompatActivity{
         setContentView(R.layout.register_view);
     }
 
+    public void gotoLogin(View view){
+        Intent intent = new Intent(RegisterView.this, LoginView.class);
+        startActivity(intent);
+    }
 
+    public void registerButtonClicked(View view){
+        txtStoreName = findViewById(R.id.etStoreName);
+        storeName = txtStoreName.getText().toString();
+        txtStoreOwnerName = findViewById(R.id.etStoreOwnerName);
+        storeOwnerName = txtStoreOwnerName.getText().toString();
+        txtUsername = findViewById(R.id.etUsername);
+        username = txtUsername.getText().toString();
+        txtPassword = findViewById(R.id.etPassword);
+        password = txtPassword.getText().toString();
+        txtConfirmPassword = findViewById(R.id.etConfirmPassword);
+        confirmPassword = txtConfirmPassword.getText().toString();
+
+        if(!registerController.checkDuplicate(userModel, storeName, storeOwnerName, username))
+            Toast.makeText(this, "Records containing this credential already exist. Use other names", Toast.LENGTH_SHORT).show();
+
+        else if(!registerController.passwordConfirmation(password, confirmPassword))
+            Toast.makeText(this, "Password does not match. Try Again", Toast.LENGTH_SHORT).show();
+
+        else{
+            registerController.registerAccount(userModel, storeName, storeOwnerName, username, password);
+            Intent intent = new Intent(RegisterView.this, LoginView.class);
+            startActivity(intent);
+        }
+
+    }
 }
 
