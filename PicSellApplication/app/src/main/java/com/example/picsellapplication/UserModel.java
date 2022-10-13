0db4c083eea7228c032299d9819bc24c57458050
rjservidor04 <73457698+rjservidor04@users.noreply.github.com>
2022-10-13 14:07:53 +0800

    UserModel updated

--- PicSellApplication/app/src/main/java/com/example/picsellapplication/UserModel.java
+++ PicSellApplication/app/src/main/java/com/example/picsellapplication/UserModel.java
@@ -1,5 +1,6 @@
 package com.example.picsellapplication;
 
+import android.content.ContentValues;
 import android.content.Context;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
@@ -16,7 +17,7 @@ public class UserModel extends SQLiteOpenHelper {
     //columns
     private static final String ID_COL = "ID";
     private static final String STORENAME_COL = "StoreName";
-    private static final String STOREOWNERNAME_COL = "OwnerName";
+    private static final String STOREOWNERNAME_COL = "StoreOwnerName";
     private static final String USERNAME_COL = "Username";
     private static final String PASSWORD_COL = "Password";
 
@@ -27,7 +28,7 @@ public class UserModel extends SQLiteOpenHelper {
 
     @Override
     public void onCreate(SQLiteDatabase userModelDB) {
-        //creating an sqlite query and setting column names along with their data types
+        //SQLite query to create a table with column names and their data type
         String query = "CREATE TABLE " + TABLENAME + " (" +
                 ID_COL + " INTEGER PRIMARY KEY AUTOINCREMENT, " +
                 STORENAME_COL + " TEXT, " +
@@ -44,9 +45,63 @@ public class UserModel extends SQLiteOpenHelper {
         onCreate(userModelDB);
     }
 
+    //creates a new account
+    public void createAccount(String storeName, String storeOwnerName, String username, String password){
+        SQLiteDatabase userModelDB = this.getWritableDatabase();
+        ContentValues recordValues = new ContentValues();
+
+        //passing all values along with its key
+        recordValues.put(STORENAME_COL, storeName);
+        recordValues.put(STOREOWNERNAME_COL, storeOwnerName);
+        recordValues.put(USERNAME_COL, username);
+        recordValues.put(PASSWORD_COL, password);
+
+        //values are then added to the DB
+        userModelDB.insert(TABLENAME, null, recordValues);
+    }
+
+    //check if the credentials matches from records stored in DB
     public boolean checkCredentialsFromDB(String username, String password){
         SQLiteDatabase userModelDB = this.getReadableDatabase();
-        Cursor cursor = userModelDB.rawQuery("Select * From Users Where Username = ?", new String[]{username});
+        Cursor cursor = userModelDB.rawQuery(
+                "Select * From Users Where Username = ? and Password = ?",
+                new String[]{username, password});
+
+        if(cursor.getCount() > 0){
+            cursor.close();
+            return true;
+        }
+
+        else{
+            cursor.close();
+            return  false;
+        }
+    }
+
+    //checks for StoreName and StoreOwnerName Duplicates
+    public boolean checkStoreDuplicateFromDB(String storeName, String storeOwnerName){
+        SQLiteDatabase userModelDB = this.getReadableDatabase();
+        Cursor cursor = userModelDB.rawQuery(
+                "Select * From Users Where StoreName = ? and StoreOwnerName = ?",
+                new String[]{storeName, storeOwnerName});
+
+        if(cursor.getCount() > 0){
+            cursor.close();
+            return true;
+        }
+
+        else{
+            cursor.close();
+            return  false;
+        }
+    }
+
+    //checks for Username Duplicates
+    public boolean checkUsernameDuplicateFromDB(String username){
+        SQLiteDatabase userModelDB = this.getReadableDatabase();
+        Cursor cursor = userModelDB.rawQuery(
+                "Select * From Users Where Username = ?",
+                new String[]{username});
 
         if(cursor.getCount() > 0){
             cursor.close();
