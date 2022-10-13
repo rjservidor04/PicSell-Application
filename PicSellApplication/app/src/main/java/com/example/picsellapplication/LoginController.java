fc7946f79f6c424cfe98a23a91c8df400841a175
rjservidor04 <73457698+rjservidor04@users.noreply.github.com>
2022-10-13 14:07:31 +0800

    Changes on Login Module

--- PicSellApplication/app/src/main/java/com/example/picsellapplication/LoginController.java
+++ PicSellApplication/app/src/main/java/com/example/picsellapplication/LoginController.java
@@ -1,19 +1,18 @@
 package com.example.picsellapplication;
 
-import android.content.Intent;
-import android.view.View;
-import android.content.Intent;
-
 public class LoginController {
-    Boolean loginStatus;
+    boolean loginStatus;
+
+    //check if fields are not empty
+    public boolean credentialsNotEmpty(String username, String password){
+       loginStatus = !username.isEmpty() && !password.isEmpty();
+
+        return loginStatus;
+    }
 
     //verify if inputted credentials exists and correct
     public boolean verifyCredentials(UserModel userModel, String username, String password){
-        if(username.isEmpty() || password.isEmpty()) loginStatus = false;
-
-        else if(!userModel.checkCredentialsFromDB(username, password)) loginStatus = false;
-
-        else loginStatus = true;
+        loginStatus = userModel.checkCredentialsFromDB(username, password);
 
         return loginStatus;
     }
--- PicSellApplication/app/src/main/java/com/example/picsellapplication/LoginView.java
+++ PicSellApplication/app/src/main/java/com/example/picsellapplication/LoginView.java
@@ -1,23 +1,18 @@
 package com.example.picsellapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
-import android.app.Activity;
 import android.content.Intent;
-import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
 import android.widget.EditText;
-import android.widget.TextView;
 import android.widget.Toast;
 
 public class LoginView extends AppCompatActivity {
 
-    LoginController controller;
-    Button loginButton;
-    String username, password;
-    EditText txtUsername, txtPassword;
     UserModel userModel = new UserModel(LoginView.this);
+    LoginController controller;
+    EditText txtUsername, txtPassword;
+    String username, password;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,26 +22,29 @@ public class LoginView extends AppCompatActivity {
 
     //when register is clicked
     public void gotoRegister(View view) {
-        Intent i = new Intent(LoginView.this, RegisterView.class);
-        startActivity(i);
+        Intent intent = new Intent(LoginView.this, RegisterView.class);
+        startActivity(intent);
     }
 
     //when login button is clicked
-    public void loginAccount(View view) {
-        loginButton = findViewById(R.id.btnLogin);
+    public void loginButtonClicked(View view) {
         txtUsername = findViewById(R.id.etUsername);
         username = txtUsername.getText().toString();
         txtPassword = findViewById(R.id.etPassword);
         password = txtPassword.getText().toString();
 
         //check if login credentials are correct
-        if(controller.verifyCredentials(userModel, username, password)){
-            Intent i= new Intent(LoginView.this, RegisterView.class);
-            startActivity(i);
+        if(!controller.credentialsNotEmpty(username, password)){
+            Toast.makeText(this, "Please fill all fields.", Toast.LENGTH_SHORT).show();
+        }
+
+        else if(controller.verifyCredentials(userModel, username, password)){
+            Intent intent= new Intent(LoginView.this, RegisterView.class);
+            startActivity(intent);
         }
 
         else{
-            Toast.makeText(this, "Login Failed. Try Again", Toast.LENGTH_SHORT).show();
+            Toast.makeText(this, "Invalid Credentials. Have you already registered?", Toast.LENGTH_SHORT).show();
         }
     }
 }
