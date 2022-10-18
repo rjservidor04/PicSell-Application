54f3f3eef7a94e68580ec0c3c97fb28b964f1ec9
rjservidor04 <73457698+rjservidor04@users.noreply.github.com>
2022-10-19 00:37:46 +0800

    Account Module UI Improvements

--- PicSellApplication/app/src/main/java/com/example/picsellapplication/RegisterView.java
+++ PicSellApplication/app/src/main/java/com/example/picsellapplication/RegisterView.java
@@ -4,12 +4,15 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.content.Intent;
 import android.view.View;
+import android.view.WindowManager;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
 public class RegisterView extends AppCompatActivity{
     UserModel userModel = new UserModel(RegisterView.this);
-    RegisterController registerController;
+    RegisterController registerController = new RegisterController();
+    Button register;
     EditText txtStoreName, txtStoreOwnerName, txtUsername, txtPassword, txtConfirmPassword;
     String storeName, storeOwnerName, username, password, confirmPassword;
 
@@ -17,6 +20,15 @@ public class RegisterView extends AppCompatActivity{
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.register_view);
+        getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_VISIBLE|WindowManager.LayoutParams.SOFT_INPUT_ADJUST_RESIZE);
+        register = findViewById(R.id.btnRegister);
+
+        register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                RegisterView.this.registerButtonClicked(view);
+            }
+        });
     }
 
     public void gotoLogin(View view){
@@ -44,8 +56,8 @@ public class RegisterView extends AppCompatActivity{
 
         else{
             registerController.registerAccount(userModel, storeName, storeOwnerName, username, password);
-            Intent intent = new Intent(RegisterView.this, LoginView.class);
-            startActivity(intent);
+            Toast.makeText(this, "Account Registered Successfully", Toast.LENGTH_SHORT).show();
+            //make an intent here
         }
 
     }
